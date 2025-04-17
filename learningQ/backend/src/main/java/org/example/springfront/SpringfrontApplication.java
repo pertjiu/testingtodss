027a1e08990335601f6cea733108dc2f71551abf
pertjiu <144671141+pertjiu@users.noreply.github.com>
2025-04-17 12:51:58 +0200

    test1

---
+++ learningQ/backend/src/main/java/org/example/springfront/SpringfrontApplication.java
@@ -0,0 +1,13 @@
+package org.example.springfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringfrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringfrontApplication.class, args);
+    }
+
+}
---
+++ learningQ/backend/src/test/java/org/example/springfront/SpringfrontApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringfrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
